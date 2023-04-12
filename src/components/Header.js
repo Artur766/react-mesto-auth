21fce394c9d32650e6290017b41aca62517a418a
Adgamov Artur <adgamoffartur@yandex.ru>
2023-04-12 20:33:24 +0300

    Добавлено: меню брургер, для мобилок

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,15 @@ import { Link, useNavigate, Route, Routes } from "react-router-dom";
 
 function Header(props) {
 
+  const [isMenu, setIsMenu] = React.useState(false);
+
+  function handleMenu() {
+    if (isMenu) {
+      setIsMenu(false);
+    }
+    else { setIsMenu(true); }
+  }
+
   const navigate = useNavigate();
 
   function signOut() {
@@ -12,29 +21,47 @@ function Header(props) {
   }
 
   return (
-    <header className="header">
-      <img className="header__logo" src={logo} alt="логотип место" />
-      <div className="header__wrapper">
-        <Routes>
-          <Route path="/" element={
-            <>
+    <Routes>
+
+      <Route path="/" element={
+        <>
+          {
+            isMenu &&
+            <div className="header__menu">
               <p className="header__email">{props.userEmail}</p>
               <Link type="button" onClick={signOut} className="header__link" to="/sign-in">Выйти</Link>
-            </>
+            </div>
           }
-          />
-          <Route path="/sign-in" element={
+          <header className="header">
+            <img className="header__logo" src={logo} alt="логотип место" />
+            {isMenu ? "" : <button className="header__menu-btn" type="button" onClick={handleMenu} />}
+            {isMenu && <button className="header__close-btn" type="button" onClick={handleMenu} />}
+            <div className="header__wrapper header__wrapper_mobile-hidden">
+              <p className="header__email">{props.userEmail}</p>
+              <Link type="button" onClick={signOut} className="header__link" to="/sign-in">Выйти</Link>
+            </div>
+          </header>
+        </>}
+      />
+      <Route path="/sign-in" element={
+        <header className="header">
+          <img className="header__logo" src={logo} alt="логотип место" />
+          <div className="header__wrapper">
             <Link type="button" onClick={signOut} className="header__link" to="/sign-up">Регистрация</Link>
-          }
-          />
-          <Route path="/sign-up" element={
+          </div>
+        </header>}
+      />
+      <Route path="/sign-up" element={
+        <header className="header">
+          <img className="header__logo" src={logo} alt="логотип место" />
+          <div className="header__wrapper">
             <Link type="button" onClick={signOut} className="header__link" to="/sign-in">Войти</Link>
-          }
-          />
-        </Routes>
-      </div>
-    </header >
+          </div>
+        </header>}
+      />
+    </Routes>
   )
 }
 
 export default Header;
+
